366a1581fb9ca35db8643a542284fc01afc789ba
mrdyman <andimardimansaputra@gmail.com>
2021-06-13 22:37:26 +0800

    Add GedungAdapter to displaying data gedung to activity

--- app/src/main/java/com/diman/sipenguji/MainActivity.kt
+++ app/src/main/java/com/diman/sipenguji/MainActivity.kt
@@ -3,17 +3,30 @@ package com.diman.sipenguji
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.diman.sipenguji.adapter.GedungAdapter
 import com.diman.sipenguji.model.DataItem
 import com.diman.sipenguji.model.Gedung
 import com.diman.sipenguji.network.ApiConfig
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: GedungAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = GedungAdapter(mutableListOf())
+
+        rv_gedung.setHasFixedSize(true)
+        rv_gedung.layoutManager = LinearLayoutManager(this)
+        rv_gedung.adapter = adapter
 
         getGedung()
     }
@@ -26,7 +39,7 @@ class MainActivity : AppCompatActivity() {
                 if (response.isSuccessful){
                     val dataArray = response.body()?.data as List<DataItem>
                     for (data in dataArray){
-                        Log.d("Hasil", data.namaGedung.toString())
+                        adapter.addGedung(data)
                     }
                 }
             }
--- app/src/main/java/com/diman/sipenguji/adapter/GedungAdapter.kt
+++ app/src/main/java/com/diman/sipenguji/adapter/GedungAdapter.kt
@@ -1,25 +1,49 @@
 package com.diman.sipenguji.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.diman.sipenguji.R
 import com.diman.sipenguji.model.DataItem
 
-class GedungAdapter (private val gedung : MutableList<DataItem>) : RecyclerView.Adapter<GedungAdapter.GedungHolder>() {
+class GedungAdapter (val gedung : MutableList<DataItem>) : RecyclerView.Adapter<GedungAdapter.GedungHolder>() {
 
+    //fungsi untuk menampilkan recyclerView saat dijalankan
+    //fungsi ini mengambil referensi layout item yang akan ditampilkan
+    // dan mengembalikannya ke kelas GedungHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GedungHolder {
-        TODO("Not yet implemented")
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_gedung, parent, false)
+        return GedungHolder(view)
     }
 
+    //fungsi holder untuk melakukan binding value yang akan di isi dari
+    //komponen yang telah di assign dari class GedungHolder
     override fun onBindViewHolder(holder: GedungHolder, position: Int) {
-        TODO("Not yet implemented")
+        val  _gedung = gedung[position]
+
+        holder.tvNamaGedung.text = _gedung.namaGedung
+        holder.tvAlamatGedung.text = _gedung.alamat
     }
 
+    //fungsi untuk hitung jumlah data
+    // dan mengembalikan jumlahnya
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return gedung.size
     }
 
+    //class yang melakukan assign(mengisi) komponen yang akan ditampilkan
+    //kedalan recyclerView
     class GedungHolder (itemView : View) : RecyclerView.ViewHolder(itemView) {
+        var tvNamaGedung: TextView = itemView.findViewById(R.id.tv_nama_gedung)
+        var tvAlamatGedung: TextView = itemView.findViewById(R.id.tv_alamat)
 
+    }
+
+    //fungsi untuk masukkan data gedung kedalam recycleview
+    fun addGedung(newDataGedung: DataItem){
+        gedung.add(newDataGedung)
+        notifyItemInserted(gedung.lastIndex)
     }
 }

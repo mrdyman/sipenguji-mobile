0656a46e4aea88baa4de42c7bb3d6effa32a151c
mrdyman <andimardimansaputra@gmail.com>
2021-06-15 22:55:02 +0800

    remove chucker debug

--- app/src/main/java/com/diman/sipenguji/network/ApiConfig.kt
+++ app/src/main/java/com/diman/sipenguji/network/ApiConfig.kt
@@ -1,14 +1,25 @@
 package com.diman.sipenguji.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiConfig {
     companion object {
         fun getApiService(): ApiService {
+            //logging interceptor untuk debug aktivitas http request di terminal
+            val loggingInterceptor =
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+
             val retrofit = Retrofit.Builder()
                 .baseUrl("http://192.168.43.26:80/sipenguji-api/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build()
             return retrofit.create(ApiService::class.java)
         }

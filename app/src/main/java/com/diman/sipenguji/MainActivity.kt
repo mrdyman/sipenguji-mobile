4c54b4b2ff9db07510ec1e9cbd7789c826f8e985
mrdyman <andimardimansaputra@gmail.com>
2021-06-17 13:07:45 +0800

    add new fragment BarcodeScanner

--- app/src/main/java/com/diman/sipenguji/MainActivity.kt
+++ app/src/main/java/com/diman/sipenguji/MainActivity.kt
@@ -6,9 +6,11 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.diman.sipenguji.adapter.GedungAdapter
+import com.diman.sipenguji.fragment.BarcodeScannerFragment
 import com.diman.sipenguji.fragment.HomeFragment
 import com.diman.sipenguji.model.DataItem
 import com.diman.sipenguji.model.Gedung
@@ -35,8 +37,36 @@ class MainActivity : AppCompatActivity() {
         bottomNavigation.add(MeowBottomNavigation.Model(3, R.drawable.ic_message))
 
         val fr = supportFragmentManager.beginTransaction()
-        fr.add(R.id.fl_recyclerView, HomeFragment())
+        fr.add(R.id.fragment_container, HomeFragment())
         fr.commit()
 
+        bottomNavigation.setOnClickMenuListener {
+            var selectedFragment: Fragment;
+            when(it.id){
+                1 -> {
+                    Log.i("Message", "Home Selected")
+                    selectedFragment = HomeFragment()
+                    replaceFragment(selectedFragment)
+                }
+
+                2 -> {
+                    Log.i("Message", "Scanner Selected")
+                    selectedFragment = BarcodeScannerFragment()
+                    replaceFragment(selectedFragment)
+                }
+
+                3 -> {
+                    Log.i("Message", "Menu Selected")
+//                    selectedFragment = HomeFragment()
+                }
+
+            }
+        }
+    }
+
+    private fun replaceFragment(frNama: Fragment){
+        val _fragment = supportFragmentManager.beginTransaction()
+        _fragment.replace(R.id.fragment_container, frNama)
+        _fragment.commit()
     }
 }

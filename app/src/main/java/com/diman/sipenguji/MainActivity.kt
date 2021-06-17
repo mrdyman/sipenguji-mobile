1ad66f5bedc0af50178a57330ca1deb6cf6554e3
mrdyman <andimardimansaputra@gmail.com>
2021-06-17 17:19:24 +0800

    create camera permission prompt when open scanner fragment

--- app/src/main/java/com/diman/sipenguji/MainActivity.kt
+++ app/src/main/java/com/diman/sipenguji/MainActivity.kt
@@ -3,25 +3,12 @@ package com.diman.sipenguji
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.diman.sipenguji.adapter.GedungAdapter
 import com.diman.sipenguji.fragment.BarcodeScannerFragment
 import com.diman.sipenguji.fragment.HomeFragment
-import com.diman.sipenguji.model.DataItem
-import com.diman.sipenguji.model.Gedung
-import com.diman.sipenguji.network.ApiConfig
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation
 import com.facebook.stetho.Stetho
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.item_gedung.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/com/diman/sipenguji/fragment/BarcodeScannerFragment.kt
+++ app/src/main/java/com/diman/sipenguji/fragment/BarcodeScannerFragment.kt
@@ -1,16 +1,88 @@
 package com.diman.sipenguji.fragment
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.diman.sipenguji.MainActivity
 import com.diman.sipenguji.R
+import com.google.common.util.concurrent.ListenableFuture
+import kotlinx.android.synthetic.main.fragment_barcode_scanner.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class BarcodeScannerFragment : Fragment() {
 
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private lateinit var cameraExecutor: ExecutorService
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireActivity())
+
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(requireActivity()))
+
+        checkCameraPermission()
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_barcode_scanner, container, false)
+    }
+
+    @SuppressLint("UnsafeExperimentalUsageError")
+    private fun bindPreview(cameraProvider: ProcessCameraProvider){
+        val preview: Preview = Preview.Builder().build()
+        val cameraSelector: CameraSelector = CameraSelector.Builder().requireLensFacing(
+            CameraSelector.LENS_FACING_BACK).build()
+        preview.setSurfaceProvider(previewView.surfaceProvider)
+
+        cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+    }
+
+    private fun checkCameraPermission() {
+        val permission = ContextCompat.checkSelfPermission(requireActivity(), android.Manifest.permission.CAMERA)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.e("Error", "Camera Permission not GRANTED!")
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.CAMERA),
+            BarcodeScannerFragment.CAMERA_REQ
+        )
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,permissions: Array<out String>,grantResults: IntArray) {
+        when (requestCode) {
+            BarcodeScannerFragment.CAMERA_REQ -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(
+                        requireActivity(),
+                        "You need the camera permission to use this app",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val CAMERA_REQ = 101
     }
 }

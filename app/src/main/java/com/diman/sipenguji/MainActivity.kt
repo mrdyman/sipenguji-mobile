9dc2f97c653ffe613697cf5b3aa46a4898555348
mrdyman <andimardimansaputra@gmail.com>
2021-06-17 12:26:19 +0800

    Add HomeFragment and move recyclerView from activity to fragment

--- app/src/main/java/com/diman/sipenguji/MainActivity.kt
+++ app/src/main/java/com/diman/sipenguji/MainActivity.kt
@@ -6,8 +6,10 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.diman.sipenguji.adapter.GedungAdapter
+import com.diman.sipenguji.fragment.HomeFragment
 import com.diman.sipenguji.model.DataItem
 import com.diman.sipenguji.model.Gedung
 import com.diman.sipenguji.network.ApiConfig
@@ -21,46 +23,20 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var adapter: GedungAdapter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Stetho.initializeWithDefaults(this);
 
-        adapter = GedungAdapter(mutableListOf())
-
-        rv_gedung.setHasFixedSize(true)
-        rv_gedung.layoutManager = LinearLayoutManager(this)
-        rv_gedung.adapter = adapter
-
-        getGedung()
-
         bottomNavigation.show(1, true)
         bottomNavigation.add(MeowBottomNavigation.Model(1, R.drawable.ic_home))
         bottomNavigation.add(MeowBottomNavigation.Model(2, R.drawable.ic_qrcode))
         bottomNavigation.add(MeowBottomNavigation.Model(3, R.drawable.ic_message))
 
-    }
+        val fr = supportFragmentManager.beginTransaction()
+        fr.add(R.id.fl_recyclerView, HomeFragment())
+        fr.commit()
 
-    fun getGedung(){
-        val client = ApiConfig.getApiService().getListGedung()
-
-        client.enqueue(object : Callback<Gedung>{
-            override fun onResponse(call: Call<Gedung>, response: Response<Gedung>) {
-                if (response.isSuccessful){
-                    val dataArray = response.body()?.data as List<DataItem>
-                    for (data in dataArray){
-                        adapter.addGedung(data)
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<Gedung>, t: Throwable) {
-                t.printStackTrace()
-            }
-
-        })
     }
 }
---
+++ app/src/main/java/com/diman/sipenguji/fragment/HomeFragment.kt
@@ -0,0 +1,62 @@
+package com.diman.sipenguji.fragment
+
+import android.app.Application
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.diman.sipenguji.R
+import com.diman.sipenguji.adapter.GedungAdapter
+import com.diman.sipenguji.model.DataItem
+import com.diman.sipenguji.model.Gedung
+import com.diman.sipenguji.network.ApiConfig
+import kotlinx.android.synthetic.main.fragment_home.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class HomeFragment : Fragment() {
+
+    private lateinit var adapter: GedungAdapter
+
+    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = GedungAdapter(mutableListOf())
+
+        rv_gedung.setHasFixedSize(true)
+        rv_gedung.layoutManager = LinearLayoutManager(activity)
+        rv_gedung.adapter = adapter
+
+        getGedung()
+    }
+
+    private fun getGedung() {
+        val client = ApiConfig.getApiService().getListGedung()
+
+        client.enqueue(object : Callback<Gedung> {
+            override fun onResponse(call: Call<Gedung>, response: Response<Gedung>) {
+                if (response.isSuccessful) {
+                    val dataArray = response.body()?.data as List<DataItem>
+                    for (data in dataArray) {
+                        adapter.addGedung(data)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<Gedung>, t: Throwable) {
+                t.printStackTrace()
+            }
+
+        })
+    }
+}

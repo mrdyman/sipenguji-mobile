c008dfae12494a76fba3e5398582925b387f6997
mrdyman <andimardimansaputra@gmail.com>
2021-07-17 14:42:00 +0800

    add function on back button

--- app/src/main/java/com/diman/sipenguji/fragment/DetailRuanganFragment.kt
+++ app/src/main/java/com/diman/sipenguji/fragment/DetailRuanganFragment.kt
@@ -6,11 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.diman.sipenguji.R
+import kotlinx.android.synthetic.main.fragment_detail_ruangan.*
 
 class DetailRuanganFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail_ruangan, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_detail_ruangan_back.setOnClickListener {
+            activity?.finish()
+        }
     }
 }
